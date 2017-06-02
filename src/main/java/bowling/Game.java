890d50e14e75a8eee6061ded330c0822afee010b
myeongju.jung <redutan@gmail.com>
2017-06-02 18:22:35 +0900

    84 페이지까지

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -17,17 +17,23 @@ public class Game {
     public void add(int pins) {
         itsThrows[itsCurrentThrow++] = pins;
         itsScore += pins;
-        adjustCurrentFrame();
+        adjustCurrentFrame(pins);
     }
 
     // 맞추다. 조정하다
-    private void adjustCurrentFrame() {
+    private void adjustCurrentFrame(int pins) {
         if (firstThrow) {
-            firstThrow = false;
+            // 스트라이크
+            if (pins == 10) {
+                itsCurrentFrame++;
+            } else {
+                firstThrow = false;
+            }
         } else {
             firstThrow = true;
             itsCurrentFrame++;
         }
+        itsCurrentFrame = Math.min(11, itsCurrentFrame);
     }
 
     public int scoreForFrame(int theFrame) {
@@ -35,13 +41,17 @@ public class Game {
         int score = 0;
         for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
             int firstThrow = itsThrows[ball++];
-            int secondThrow = itsThrows[ball++];
-            int frameScore = firstThrow + secondThrow;
-            // 스페어는 다음 프레임의 첫번째 투구에 필요하다.
-            if (frameScore == 10) {
-                score += frameScore + itsThrows[ball];
+            if (firstThrow == 10) {
+                score += 10 + itsThrows[ball] + itsThrows[ball + 1];
             } else {
-                score += frameScore;
+                int secondThrow = itsThrows[ball++];
+                int frameScore = firstThrow + secondThrow;
+                // 스페어는 다음 프레임의 첫번째 투구에 필요하다.
+                if (frameScore == 10) {
+                    score += frameScore + itsThrows[ball];
+                } else {
+                    score += frameScore;
+                }
             }
         }
         return score;
--- src/test/java/bowling/TestGame.java
+++ src/test/java/bowling/TestGame.java
@@ -56,4 +56,79 @@ public class TestGame {
         assertEquals(18, g.score());
         assertEquals(3, g.getCurrentFrame());
     }
+
+    @Test
+    public void testSimpleStrike() throws Exception {
+        g.add(10);
+        g.add(3);
+        g.add(6);
+        assertEquals(19, g.scoreForFrame(1));
+        assertEquals(28, g.score());
+        assertEquals(3, g.getCurrentFrame());
+    }
+
+    @Test
+    public void testPerfectGame() throws Exception {
+        for (int i = 0; i < 12; i++) {
+            g.add(10);
+        }
+        assertEquals(300, g.score());
+        assertEquals(11, g.getCurrentFrame());
+    }
+
+    @Test
+    public void testEndOfArray() throws Exception {
+        for (int i = 0; i < 9; i++) {
+            g.add(0);
+            g.add(0);
+        }
+        g.add(2);
+        g.add(8);   // 10번째 프레임의 스페어
+        g.add(10);  // 배열 마지막 위치에서의 스트라이크
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void testSampleGame() throws Exception {
+        g.add(1);
+        g.add(4);
+        g.add(4);
+        g.add(5);
+        g.add(6);
+        g.add(4);
+        g.add(5);
+        g.add(5);
+        g.add(10);
+        g.add(0);
+        g.add(1);
+        g.add(7);
+        g.add(3);
+        g.add(6);
+        g.add(4);
+        g.add(10);
+        g.add(2);
+        g.add(8);
+        g.add(6);
+        assertEquals(133, g.score());
+    }
+
+    @Test
+    public void testHeartBreak() throws Exception {
+        for (int i = 0; i < 11; i++) {
+            g.add(10);
+        }
+        g.add(9);
+        assertEquals(299, g.score());
+    }
+
+    @Test
+    public void testTenthFrameSpare() throws Exception {
+        for (int i = 0; i < 9; i++) {
+            g.add(10);
+        }
+        g.add(9);
+        g.add(1);
+        g.add(1);
+        assertEquals(270, g.score());
+    }
 }
